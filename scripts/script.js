866a320ac73e36b1c4ca93cf24a2b824e1f84ba5
Mikhail Pimenov <tomorrowind@ya.ru>
2020-06-22 12:35:00 +0300

    Fix first review comments

--- scripts/script.js
+++ scripts/script.js
@@ -1,47 +1,61 @@
-function togglePopup() {
-  let popup = document.querySelector(".popup");
-  popup.classList.toggle("popup_opened");
-  initForm();
-}
+// *popup elements
+let popup = document.querySelector(".popup");
+let popupCloseBtn = document.querySelector(".popup__close-btn");
+let popupFullName = document.querySelector(".popup__full-name");
+let popupDescription = document.querySelector(".popup__description");
 
+// *profile elements
+let profileEditBtn = document.querySelector(".profile__edit-btn");
+let profileFullName = document.querySelector(".profile__full-name");
+let profileDescription = document.querySelector(".profile__description");
+
+// *function declarations
+
+/**
+ * Get profile information
+ */
 function getProfile() {
   return {
-    fullName: document.querySelector(".profile__full-name").textContent.trim(),
-    description: document
-      .querySelector(".profile__description")
-      .textContent.trim(),
+    fullName: profileFullName.textContent.trim(),
+    description: profileDescription.textContent.trim(),
   };
 }
 
+/**
+ * Initislaize form
+ */
 function initForm() {
   let profile = getProfile();
 
-  document.querySelector(".popup__full-name").value = profile.fullName;
-  document.querySelector(".popup__description").value = profile.description;
+  popupFullName.value = profile.fullName;
+  popupDescription.value = profile.description;
 }
 
-function updateProfile(profile) {
-  document.querySelector(".profile__full-name").textContent = profile.fullName;
-  document.querySelector(".profile__description").textContent =
-    profile.description;
+/**
+ *  Open/close popup form
+ */
+function togglePopup() {
+  popup.classList.toggle("popup_opened");
+
+  if (popup.classList.contains("popup_opened")) {
+    initForm();
+  }
 }
 
+/**
+ * Handle submit event for popup-save button
+ * @param {object} evt contanin event infomration
+ */
 function formSubmitHandler(evt) {
   evt.preventDefault();
 
-  updateProfile({
-    fullName: document.querySelector(".popup__full-name").value.trim(),
-    description: document.querySelector(".popup__description").value.trim(),
-  });
+  profileFullName.textContent = popupFullName.value.trim();
+  profileDescription.textContent = popupDescription.value.trim();
 
   togglePopup();
 }
 
-let profileEditBtn = document.querySelector(".profile__edit-btn");
-profileEditBtn.addEventListener("click", togglePopup);
-
-let popupCloseBtn = document.querySelector(".popup__close-btn");
+// *main
 popupCloseBtn.addEventListener("click", togglePopup);
-
-let popup = document.querySelector(".popup");
+profileEditBtn.addEventListener("click", togglePopup);
 popup.addEventListener("submit", formSubmitHandler);
