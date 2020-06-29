17452f828e41a4f918c290dc4820171af7d9dd11
Mikhail Pimenov <tomorrowind@ya.ru>
2020-06-29 18:07:28 +0300

    feat: add photo popup

--- scripts/script.js
+++ scripts/script.js
@@ -27,22 +27,27 @@ const initialCards = [
 ];
 
 // *popup elements
-const popup = document.querySelector(".popup_type_edit-profile");
-const popupCloseBtn = popup.querySelector(".popup__close-btn");
-const popupFullName = popup.querySelector(".popup__input_el_full-name");
-const popupDescription = popup.querySelector(".popup__input_el_description");
+const popup = document.querySelector('.popup_type_edit-profile');
+const popupCloseBtn = popup.querySelector('.popup__close-btn');
+const popupFullName = popup.querySelector('.popup__input_el_full-name');
+const popupDescription = popup.querySelector('.popup__input_el_description');
 
 
-const popupPlace = document.querySelector(".popup_type_new-place");
-const popupPlaceCloseBtn = popupPlace.querySelector(".popup__close-btn");
-const popupPlaceName = popupPlace.querySelector(".popup__input_el_place-name");
-const popupPlaceLink = popupPlace.querySelector(".popup__input_el_place-link");
+const popupPlace = document.querySelector('.popup_type_new-place');
+const popupPlaceCloseBtn = popupPlace.querySelector('.popup__close-btn');
+const popupPlaceName = popupPlace.querySelector('.popup__input_el_place-name');
+const popupPlaceLink = popupPlace.querySelector('.popup__input_el_place-link');
+
+const popupPhoto = document.querySelector('.popup_type_photo');
+const popupPhotoCloseBtn = popupPhoto.querySelector('.popup__close-btn');
+const popupImage = popupPhoto.querySelector('.popup__photo');
+const popupPhotoDescription = popupPhoto.querySelector('.popup__photo-description');
 
 // *profile elements
 const profileAddBtn = document.querySelector('.profile__add-btn')
-const profileEditBtn = document.querySelector(".profile__edit-btn");
-const profileFullName = document.querySelector(".profile__full-name");
-const profileDescription = document.querySelector(".profile__description");
+const profileEditBtn = document.querySelector('.profile__edit-btn');
+const profileFullName = document.querySelector('.profile__full-name');
+const profileDescription = document.querySelector('.profile__description');
 
 //* place elements
 const places = document.querySelector('.places');
@@ -77,11 +82,24 @@ function initPlaceForm() {
   popupPlaceLink.value = '';
 }
 
+function initPhotoForm(photo) {
+  popupImage.src = photo.imageURL;
+  popupPhotoDescription.textContent = photo.description;
+}
+
+function placePhotoClickHandler(evt) {
+  const imageURL = evt.target.src;
+  const description = evt.target.closest('.place').querySelector('.place__name').textContent.trim();
+
+  initPhotoForm({ imageURL, description });
+  togglePopup(popupPhoto);
+}
+
 /**
  *  Open/close popup form
  */
 function togglePopup(popupElement) {
-  popupElement.classList.toggle("popup_opened");
+  popupElement.classList.toggle('popup_opened');
 }
 
 /**
@@ -112,17 +130,24 @@ function toggleLike(likeElement) {
   likeElement.classList.toggle('place__like_checked');
 }
 
+function removePlace(removePlaceBtn) {
+  removePlaceBtn.closest('.place').remove();
+}
+
 
 function addPlace(place) {
   const placeElement = document.querySelector('#place-template').content.cloneNode(true);
   const placePhoto = placeElement.querySelector('.place__photo');
   const placeLike = placeElement.querySelector('.place__like');
   const placeName = placeElement.querySelector('.place__name');
+  const placeRemoveBtn = placeElement.querySelector('.place__remove-btn');
 
 
   placePhoto.src = place.imageURL;
   placeName.textContent = place.name;
   placeLike.addEventListener('click', e => toggleLike(e.target));
+  placeRemoveBtn.addEventListener('click', e => removePlace(e.target));
+  placePhoto.addEventListener('click', placePhotoClickHandler);
 
   places.prepend(placeElement);
 
@@ -134,15 +159,16 @@ function initPlaces(...places) {
 
 
 // *main
-profileEditBtn.addEventListener("click", e => togglePopup(popup));
-profileEditBtn.addEventListener("click", initProfileForm);
-popupCloseBtn.addEventListener("click", e => togglePopup(popup));
-popup.addEventListener("submit", formSubmitHandler);
+profileEditBtn.addEventListener('click', e => togglePopup(popup));
+profileEditBtn.addEventListener('click', initProfileForm);
+popupCloseBtn.addEventListener('click', e => togglePopup(popup));
+popup.addEventListener('submit', formSubmitHandler);
 
 profileAddBtn.addEventListener('click', e => togglePopup(popupPlace));
 profileAddBtn.addEventListener('click', initPlaceForm)
-popupPlaceCloseBtn.addEventListener("click", e => togglePopup(popupPlace));
+popupPlaceCloseBtn.addEventListener('click', e => togglePopup(popupPlace));
 popupPlace.addEventListener('submit', formPlaceSubmitHandler);
+popupPhotoCloseBtn.addEventListener('click', e => togglePopup(popupPhoto));
 
 
 initPlaces(...initialCards);
